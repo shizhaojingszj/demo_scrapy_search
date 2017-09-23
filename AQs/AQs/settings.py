ff1210fd66c8b0ed22fc618a27af2f1b7d539059
zhaomeng <shizhaojingszj@163.com>
2017-09-23 13:33:22 +0800

    fixes #6

--- AQs/AQs/settings.py
+++ AQs/AQs/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'AQs.spiders'
 #USER_AGENT = 'AQs (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
