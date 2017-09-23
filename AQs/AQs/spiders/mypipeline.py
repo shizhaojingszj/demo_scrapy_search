456a72ae876395a285860b6a481f84aef59db6d6
zhaomeng <shizhaojingszj@163.com>
2017-09-23 17:37:10 +0800

    fixes #7

---
+++ AQs/AQs/spiders/mypipeline.py
@@ -0,0 +1,19 @@
+import json
+import codecs
+
+
+class JsonLinePipeline(object):
+
+    def open_spider(self, spider):
+
+        self.file = codecs.open("test.jl", 'w', encoding="utf-8")
+
+    def close_spider(self, spider):
+
+        self.file.close()
+
+    def process_item(self, item, spider):
+
+        line = json.dumps(dict(item), ensure_ascii=False) + "\n"
+        self.file.write(line)
+        return item
